7eb1ab20440f46bdac8fb06a0022a2c5ca61199a
GustavoKurek <gustavokureksz@gmail.com>
2021-11-02 19:34:09 -0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+  },
+};
