1ce7bf7f24e9e59df981bd32c186942506b1308f
GustavoKurek <gustavokureksz@gmail.com>
2021-11-06 20:19:03 -0300

    commit CRUD usuários

--- src/controllers/HomeControler.js
+++ src/controllers/HomeControler.js
@@ -2,15 +2,7 @@ import Aluno from '../models/Aluno';
 
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'gustavo',
-      sobrenome: 'Kurek',
-      email: 'gustavokureksz@gmail.com',
-      idade: 22,
-      peso: 80,
-      altura: 1.80,
-
-    });
+    const novoAluno = await Aluno.create(req.body);
     res.json(novoAluno);
   }
 }
--- src/controllers/UserControler.js
+++ src/controllers/UserControler.js
@@ -1,14 +1,90 @@
 import User from '../models/Users';
 
 class UserController {
+  // store
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (e) {
-      res.status(400).json({ errors: e.errors.map((err) => err.message) });
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  // index
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  // show
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  // update
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Id não enviado'],
+        });
+      }
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ['usuário não existe'],
+        });
+      }
+      const novosDados = await user.update(req.body);
+
+      return res.json(novosDados);
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
+    }
+  }
+
+  // delete
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Id não enviado'],
+        });
+      }
+      const user = await User.findByPk(id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ['usuário não existe'],
+        });
+      }
+      await user.destroy();
+
+      return res.json(user);
+    } catch (e) {
+      return res.status(400).json({ errors: e.errors.map((err) => err.message) });
     }
   }
 }
+
+/*
+index => lista todos os usuários -> get
+store/create => cria no usuário -> post
+delete -> apaga um usuário -> delete
+show -> mostra um usuário -> get
+update -> atualiza um usuário -> path ou put
+*/
 
 export default new UserController();
