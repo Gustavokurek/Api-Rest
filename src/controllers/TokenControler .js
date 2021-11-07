68922430ae4cc71d5e0ee065f111eb81167c61e3
GustavoKurek <gustavokureksz@gmail.com>
2021-11-06 21:33:31 -0300

    commit inicio JwtControler

---
+++ src/controllers/TokenControler .js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/Users';
+
+class TokenController {
+  async store(req, res) {
+    const { email = '', password = '' } = req.body;
+    if (!email || !password) {
+      return res.status(401).json({
+        errors: ['credencias inválidas'],
+      });
+    }
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(401).json({
+        errors: ['credencias inválidas'],
+      });
+    }
+
+    if (!(await user.pass(password))) {
+      return res.status(401).json({
+        errors: ['senha inválida'],
+      });
+    }
+    const { id } = user;
+    const token = jwt.sign({ id, email }, process.env.TOKEN_SECRET, {
+      expiresIn: process.env.TOKEN_EXP,
+    });
+    return res.json({ token });
+  }
+}
+
+export default new TokenController();
