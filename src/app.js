0b4b30d9619254ea9e82507b872110ce8ac01d9e
Gustavo Kurek <GustavoKureksz@gmail.com>
2021-11-23 21:44:54 -0300

    modificações

--- src/app.js
+++ src/app.js
@@ -21,14 +21,14 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors());
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use('/images/', express.static(resolve(__dirname, '..', 'uploads', 'images')));
   }
 
   routes() {
-    this.app.use(cors());
-    this.app.use(helmet());
     this.app.use('/', homeRoutes);
     this.app.use('/users', userRoutes);
     this.app.use('/tokens', TokenRoutes);
