18b84a14f443c22b13df037c764c949f13034639
Gustavo Kurek <GustavoKureksz@gmail.com>
2022-01-14 21:13:01 -0300

    page alunos

--- frontend/src/pages/Alunos/styled.js
+++ frontend/src/pages/Alunos/styled.js
@@ -1,15 +1,20 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-  background: green;
-
-  small {
-    font-size: 12pt;
-    margin-left: 15px;
-    color: #999;
+export const AlunoContainer = styled.div`
+  margin-top: 20px;
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5px 0;
   }
+  div + div{
+    border-top: 1px solid #edd;
 `;
-
-export const Paragrafo = styled.p`
-  font-size: 80px;
+export const ProfilePicture = styled.div`
+  img {
+    width: 36px;
+    height: 36px;
+    border-radius: 50%;
+  }
 `;
